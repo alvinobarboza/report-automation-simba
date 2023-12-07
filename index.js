ac9db8c000fab0a9fc7069ff16b99f977b95d3ea
Alvino Barboza <alvinobarboza@gmail.com>
2023-12-07 11:56:44 -0300

    Report method

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import * as SIMBA from './reportDownload/simba.js';
 import { validateSimbaReport } from './validations/simbaValidation.js';
+import { generateExcelFiles } from './report/simbaReport.js';
 
 (async function () {
     const ids = await SIMBA.returnLatestIdFromReportType();
     const downloadedReports = await SIMBA.downloadAllReports(ids);
     const downloadedReportsCSVLines =
         SIMBA.linesfromDownloadReports(downloadedReports);
-
     const validatedUsers = validateSimbaReport(downloadedReportsCSVLines);
+    generateExcelFiles(validatedUsers, downloadedReportsCSVLines);
 })();

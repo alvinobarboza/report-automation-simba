379916fcab7d7d4def2c0ff0583f2be900e71db8
Alvino Barboza <alvinobarboza@gmail.com>
2023-12-07 11:56:29 -0300

    Helper for styling

---
+++ utils/excelStyles.js
@@ -0,0 +1,339 @@
+export const headerStyle = {
+    alignment: {
+        horizontal: ['center'],
+        vertical: ['center'],
+    },
+    font: {
+        color: '#000000',
+        bold: true,
+        size: 12,
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#ffff00',
+        fgColor: '#ffff00',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const headerStyleSimba = {
+    alignment: {
+        horizontal: ['center'],
+        vertical: ['center'],
+    },
+    font: {
+        color: '#000000',
+        bold: true,
+        name: 'Times New Roman',
+        underline: true,
+        size: 12,
+    },
+    border: {
+        left: {
+            style: 'medium',
+            color: '#000000',
+        },
+        right: {
+            style: 'medium',
+            color: '#000000',
+        },
+        top: {
+            style: 'medium',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'medium',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyle = {
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#d9e1f2',
+        fgColor: '#d9e1f2',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleSimba = {
+    font: {
+        name: 'Times New Roman',
+    },
+    border: {
+        left: {
+            style: 'medium',
+            color: '#000000',
+        },
+        right: {
+            style: 'medium',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleSimbaProviders = {
+    font: {
+        name: 'Times New Roman',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleError = {
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#ec5120',
+        fgColor: '#ec5120',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleOK = {
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#a9d08e',
+        fgColor: '#a9d08e',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const headerStyleException = {
+    alignment: {
+        horizontal: ['center'],
+        shrinkToFit: true,
+        vertical: ['center'],
+    },
+    font: {
+        color: '#ffffff',
+        size: 12,
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#2d4154',
+        fgColor: '#2d4154',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleException1 = {
+    alignment: {
+        horizontal: ['right'],
+        shrinkToFit: true,
+        vertical: ['center'],
+    },
+    font: {
+        color: '#000000',
+        size: 11,
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#f3f3f3',
+        fgColor: '#f3f3f3',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleException2 = {
+    alignment: {
+        horizontal: ['center'],
+        vertical: ['center'],
+    },
+    font: {
+        color: '#000000',
+        size: 11,
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#f3f3f3',
+        fgColor: '#f3f3f3',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
+
+export const dataStyleException3 = {
+    alignment: {
+        horizontal: ['left'],
+        shrinkToFit: true,
+        vertical: ['center'],
+    },
+    font: {
+        color: '#000000',
+        size: 11,
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        bgColor: '#f3f3f3',
+        fgColor: '#f3f3f3',
+    },
+    border: {
+        left: {
+            style: 'thin',
+            color: '#000000',
+        },
+        right: {
+            style: 'thin',
+            color: '#000000',
+        },
+        top: {
+            style: 'thin',
+            color: '#000000',
+        },
+        bottom: {
+            style: 'thin',
+            color: '#000000',
+        },
+    },
+};
